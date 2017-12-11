8878a77cdcfda722f25074d2ccb0f2a4727f1076
gubelli <14937690+gubelli@users.noreply.github.com>
2017-12-11 11:07:33 +0100

    WritetoFile

--- taskList.hs
+++ taskList.hs
@@ -1,3 +1,6 @@
+import Data.List
+import System.IO
+
 -- (ID,Name)
 type Task = (Int,String)
 
@@ -15,6 +18,7 @@ getInput tasks = do
 process :: String -> [Task] -> IO ()
 process ('+':' ':task) tasks = getInput $ convert task tasks
 process ('-':' ':num ) tasks = getInput $ remove (read num) tasks
+process "s"            tasks = writeToFile tasks
 process  "q"           tasks = return ()
 process "?"            tasks = do
     showAvailableCommands
@@ -34,8 +38,20 @@ showAvailableCommands = do
     putStrLn "Commands:"
     putStrLn "+ <Title>    - Add a Task"
     putStrLn "- <TaskNr>   - Delete Task"
+    putStrLn "s            - Save task list to file"
     putStrLn "q            - Quit"
     putStrLn "?            - Show this help"
+
+writeToFile:: [Task] -> IO()
+writeToFile [] = getInput []
+writeToFile tasks = do
+    fileref <- openFile "task.txt" WriteMode 
+    hPutStrLn fileref (writeTask (head tasks))
+    hClose fileref
+    getInput tasks
+
+writeTask:: Task -> String
+writeTask (id,task) = "ID: " ++ show id ++ " " ++ task
 
 main = do
     showAvailableCommands
